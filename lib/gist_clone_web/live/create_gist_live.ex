6913940e22a048c8b02f68c45d1802cb67e65378
edkahara <edward@ganzdigital.com>
2024-12-01 15:04:42 +0300

    create gist form

--- lib/gist_clone_web/live/create_gist_live.ex
+++ lib/gist_clone_web/live/create_gist_live.ex
@@ -1,7 +1,18 @@
 defmodule GistCloneWeb.CreateGistLive do
   use GistCloneWeb, :live_view
 
+  import Phoenix.HTML
+  import Phoenix.HTML.Form
+  use PhoenixHTMLHelpers
+
+  alias GistClone.{Gists, Gists.Gist}
+
   def mount(_params, _session, socket) do
+    socket = assign(
+      socket,
+      form: to_form(Gists.change_gist(%Gist{}))
+    )
+
     {:ok, socket}
   end
 end
